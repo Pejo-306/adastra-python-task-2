3e62807c43e7d3cc2a5e1d0cdcaf05027cdbf433
Petar Nikolov <penikolov23@gmail.com>
2021-07-29 13:57:57 +0300

    Document to fahrenheit transformation function

--- src/transform.py
+++ src/transform.py
@@ -2,6 +2,16 @@ from fractions import Fraction
 
 
 def to_fahrenheit(temperature: str) -> str:
+    """Convert Celsius temperature value to Fahrenheit
+
+    If the input temperature value is already in Fahrenheit, it is only formatted
+    and returned as is.
+
+    :param temperature: input temperature, either in Celsius or Fahrenheit
+    :type temperature: str
+    :return: formatted temperature value in Fahrenheit
+    :rtype: str
+    """
     temperature = temperature.strip()
     degrees = temperature[:3] if temperature[0] in ('+', '-') else temperature[:2]
     sign = temperature[-1]
